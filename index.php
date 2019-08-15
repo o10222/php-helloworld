c5afbe2e292679cdc897e57a71f9bc79953b901c
o10222 <aafa7600@cox.net>
2019-08-15 11:23:16 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-print " Hello from that lates change in PHP-Helloworld  php version is " . PHP_VERSION . "\n";
+print " Hello from that latest change in PHP-Helloworld  php version is " . PHP_VERSION . "\n";
 print " Changes Have been made to 8 Chan \n";
 ?>
