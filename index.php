625fda2c9dff10eb43a88c4d7206f39844793f43
o10222 <aafa7600@cox.net>
2019-08-15 11:22:57 -0700

    fixed bad spelleing

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 print " Hello from that lates change in PHP-Helloworld  php version is " . PHP_VERSION . "\n";
-print " Changes Have bben made to 8 Chan \n";
+print " Changes Have been made to 8 Chan \n";
 ?>
